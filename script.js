393850fd708dd05cf3d035819bba623a3d478285
aukewor <60113380+aukewor@users.noreply.github.com>
2020-04-26 13:21:34 -0400

    emoji update

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var templateIndex = 0;
+function template() {
+  var x = document.getElementsByClassName ("template");
+  for ( var i = 0; i < x.length; i++ ) {
+    x[i].style.display = "none";
+  }
+  templateIndex++;
+  if (templateIndex > x.length) {templateIndex = 1}
+  x[templateIndex-1].style.display = "block";
+  setTimeout(template,160);
+}
